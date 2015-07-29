272d63971000afbe976b88a06728919b8f27703f
Miguel Peixe <miguel@cardume.art.br>
2015-07-29 16:00:34 -0300

    add hooks examples

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php
 
-function blank_jeo_scripts() {
+/*
+ * Clears JEO default front-end styles and scripts
+ */
+function jeo_blank_scripts() {
 
 	// deregister jeo styles
 	wp_deregister_style('jeo-main');
@@ -9,4 +12,50 @@ function blank_jeo_scripts() {
   wp_deregister_script('jeo-site');
 
 }
-add_action('wp_enqueue_scripts', 'blank_jeo_scripts', 10);
+add_action('wp_enqueue_scripts', 'jeo_blank_scripts', 10);
+
+/*
+ * JEO Hooks examples
+ * Most common used hooks
+ */
+
+// Action right after JEO functionality inits
+function jeo_blank_init() {
+  // Action goes here
+}
+add_action('jeo_init', 'jeo_blank_init');
+
+// Hook scripts after JEO scripts has been initialized
+function jeo_blank_jeo_scripts() {
+
+  // Register and enqueue scripts here
+
+}
+add_action('jeo_enqueue_scripts', 'jeo_blank_jeo_scripts');
+
+// Filter to change posts GeoJSON data (also changes the GeoJSON API output)
+function jeo_blank_marker_data($data, $post) {
+
+  // Change $data here
+
+  return $data;
+}
+add_filter('jeo_marker_data', 'jeo_blank_marker_data', 10, 2);
+
+// Filter to change GeoJSON response
+function jeo_blank_markers_data($data, $query) {
+
+  // Change $data here
+
+  return $data;
+}
+add_filter('jeo_markers_data', 'jeo_blank_markers_data', 10, 2);
+
+// Filter to programatically change map data
+function jeo_blank_map_data($data, $map) {
+
+  // Change $data here
+
+  return $data;
+}
+add_filter('jeo_map_data', 'jeo_blank_map_data'), 10, 2);
