915ad09ae48c96cc329735a79475319a9bcc8029
Miguel Peixe <miguel@cardume.art.br>
2015-10-05 13:38:11 -0300

    add tax name to archive header

--- archive.php
+++ archive.php
@@ -19,19 +19,39 @@ get_header(); ?>
 
 	<div id="content" class="site-content" role="main">
 
-		<h1 class="archive-title"><?php
-				if( is_tag() || is_category() || is_tax() ) :
-					printf( __( '%s', 'jeo' ), single_term_title() );
-				elseif ( is_day() ) :
-					printf( __( 'Daily Archives: %s', 'jeo' ), get_the_date() );
-				elseif ( is_month() ) :
-					printf( __( 'Monthly Archives: %s', 'jeo' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'jeo' ) ) );
-				elseif ( is_year() ) :
-					printf( __( 'Yearly Archives: %s', 'jeo' ), get_the_date( _x( 'Y', 'yearly archives date format', 'jeo' ) ) );
-				else :
-					_e( 'Archives', 'jeo' );
-				endif;
-			?></h1>
+		<header class="archive-header">
+			<?php if(is_tag() || is_category() || is_tax()) : ?>
+				<p class="tax-name">
+					<?php
+					if(is_tax()) {
+						global $wp_query;
+						$tax = get_taxonomy($wp_query->query_vars['taxonomy']);
+					} elseif(is_category()) {
+						$tax = get_taxonomy('category');
+					} elseif(is_tag()) {
+						$tax = get_taxonomy('post_tag');
+					}
+					echo $tax->labels->singular_name;
+					?>
+				</p>
+			<?php endif; ?>
+
+			<h1 class="archive-title">
+				<?php
+					if( is_tag() || is_category() || is_tax() ) :
+						printf( __( '%s', 'newsroom' ), single_term_title() );
+					elseif ( is_day() ) :
+						printf( __( 'Daily Archives: %s', 'newsroom' ), get_the_date() );
+					elseif ( is_month() ) :
+						printf( __( 'Monthly Archives: %s', 'newsroom' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'newsroom' ) ) );
+					elseif ( is_year() ) :
+						printf( __( 'Yearly Archives: %s', 'newsroom' ), get_the_date( _x( 'Y', 'yearly archives date format', 'newsroom' ) ) );
+					else :
+						_e( 'Archives', 'newsroom' );
+					endif;
+				?>
+			</h1>
+		</header>
 
     <div class="row">
       <?php jeo_featured(); ?>
