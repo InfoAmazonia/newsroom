5f7558467b8e7d3a1d11a93433b8da2fc42cba6b
Miguel Peixe <miguel@cardume.art.br>
2015-10-11 20:56:01 -0300

    fix search form

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
     <div>
-    	<input type="text" name="s" id="s" placeholder="<?php _e('Search here...', 'jeo'); ?>" value="<?php if(isset($_GET['s'])) echo $_GET['s']; ?>" />
+    	<input type="text" name="s" id="s" placeholder="<?php _e('Search here...', 'newsroom'); ?>" value="<?php if(isset($_GET['s'])) echo $_GET['s']; ?>" />
     </div>
 </form>
