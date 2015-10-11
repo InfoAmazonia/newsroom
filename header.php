736cc7860b1541d00e7d5eba39b55d0458b6b0b5
Miguel Peixe <miguel@cardume.art.br>
2015-10-11 20:52:18 -0300

    add social links

--- header.php
+++ header.php
@@ -31,13 +31,31 @@
 			<div class="site-meta">
 				<?php newsroom_logo(); ?>
 			</div>
-			<nav id="langnav">
-				<?php
-				if(function_exists('qtranxf_generateLanguageSelectCode')) {
-					echo qtranxf_generateLanguageSelectCode('text');
-				}
-				?>
-			</nav>
+			<div class="top-nav">
+				<nav id="langnav">
+					<?php
+					if(function_exists('qtranxf_generateLanguageSelectCode')) {
+						echo qtranxf_generateLanguageSelectCode('text');
+					}
+					?>
+				</nav>
+				<nav id="socialnav">
+					<?php
+					$fb = newsroom_get_facebook_url();
+					$tw = newsroom_get_twitter_url();
+					if($fb) :
+						?>
+						<a href="<?php echo $fb; ?>" rel="external" title="Facebook" class="icon icon-facebook"></a>
+						<?php
+					endif;
+					if($tw) :
+						?>
+						<a href="<?php echo $tw; ?>" rel="external" title="Twitter" class="icon icon-twitter"></a>
+						<?php
+					endif;
+					?>
+				</nav>
+			</div>
 		</div>
 		<div>
 			<nav id="mastnav">
